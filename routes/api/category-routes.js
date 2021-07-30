3fb0c2337ff78d085b52e5527eda387f1dfc54bb
Eddie Yeung <eycs0317@gmail.com>
2021-07-29 19:51:54 -0700

    done with routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -32,6 +32,7 @@ router.get('/:id', async (req, res) => {
       attributes: ['id', 'category_name'],
       include: {model: Product, attributes: ['id', 'product_name', 'price', 'stock', 'category_id']}
     })
+    console.log('data---', data)
     if(!data) {
       res.status(404).json({message: 'No data found!!'})
       return;
@@ -44,16 +45,55 @@ router.get('/:id', async (req, res) => {
 
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post('/', async (req, res) => {
+
+    try {
+      var respond = await Category.create(req.body)
+      if (!respond) {
+        res.status(400).json({message: 'Error!!'})
+      }
+      res.status(200).json(respond)
+    }
+    catch {
+      res.status(400).json(err);
+    }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+router.put('/:id', async (req, res) => {
+  let id = req.params.id;
+  let body = req.body;
+  try {
+    let respond = await Category.update(body, {
+      where: {id: id}
+    })
+    //respond [0] / [1]
+    if(respond[0] === 0) {
+      res.status(400).json({message: ' Update Failed!!!'})
+      return;
+    }
+    res.status(200).json({message: ' Updated!!!'})
+  }
+  catch (err){
+    res.status(400).json(err)
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+
+router.delete('/:id', async (req, res) => {
+  let id = req.params.id;
+  try {
+    let respond = await Category.destroy({
+      where: {id: id}
+    })
+    if(!respond) {
+      res.status(400).json({message: ' Delete Failed!!!'})
+      return;
+    }
+    res.status(200).json('Deleted')
+  }
+  catch (err) {
+    res.status(400).json(err)
+  }
 });
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,26 +3,94 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
+router.get('/', async (req, res) => {
+    // find all tags
   // be sure to include its associated Product data
+  try {
+    var data = await Tag.findAll({
+      include: [
+        {model: Product, attributes: ['id', 'product_name', 'price', 'stock', 'category_id']}
+      ]
+    })
+    if(!data) {
+      res.status(400).json({message: 'No product found!!'})
+      return;
+    }
+    res.status(200).json(data)
+  }
+  catch {
+    res.status(400).json(data)
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  let id = req.params.id;
+  try {
+    var data = await Tag.findByPk(id, {
+      include: {model: Product}
+    });
+    if(!data) {
+      res.status(404).json({message: 'No data found!!'})
+      return;
+    }
+    res.status(200).json(data);
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+//create
+router.post('/', async (req, res) => {
   // create a new tag
+  let body = req.body;
+  try {
+    let respond = await Tag.create(body);
+    console.log('respond-----', respond)
+    res.status(200).json({message: 'Created!!'})
+  }
+  catch (err) {
+    res.status(400).json(err)
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  let body = req.body;
+  let id = req.params.id;
+  try {
+    let respond = await Tag.update(body, {
+      where: {id: id}
+    })
+    if(respond[0] === 0) {
+      res.status(400).json({message: ' Update Failed!!!'});
+      return;
+    }
+    res.status(200).json({message: 'Updated!!!!!'})
+  }
+  catch (err) {
+    res.status(400).json(err)
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+router.delete('/:id', async (req, res) => {
+  let id = req.params.id;
+  console.log('idddddd------', id)
+  try {
+    let respond = await Tag.destroy({
+      where: {id: id}
+    })
+    if(!respond) {
+      res.status(402).json({message: ' Delete Failed!!!'})
+      return;
+    }
+    res.status(200).json('Deleted')
+  }
+  catch (err) {
+    res.status(401).json(err)
+  }
 });
 
 module.exports = router;
